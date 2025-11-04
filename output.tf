50c7d3595af124f7cb5138b927b2bfdbbee889d8
Maruti Chavan <chavan.maruti2307@gmail.com>
2025-11-04 21:55:33 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds.address
+}
